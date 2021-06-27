63555607f7fadb41ba3d46afc1ec226c97d01452
John <joaop.gomes123@gmail.com>
2021-06-27 12:25:43 -0300

    add filter stocks by country + tests

--- stocks/collector.py
+++ stocks/collector.py
@@ -1,6 +1,8 @@
 # -*- coding: utf-8 -*-
 from selenium import webdriver
 from selenium.webdriver.firefox.options import Options
+from selenium.webdriver.firefox.webelement import FirefoxWebElement
+# from selenium.webdriver.common.desired_capabilities import DesiredCapabilities
 
 
 class YahooCollector:
@@ -10,7 +12,11 @@ class YahooCollector:
     def __init__(self):
         options = Options()
         options.add_argument("--headless")
-        self.driver = webdriver.Firefox(options=options)
+        # caps = DesiredCapabilities.FIREFOX
+        # caps['marionette'] = False
+        self.driver = webdriver.Firefox(webdriver.FirefoxProfile(), options=options)  # (options=options)
+
+    def open_site(self):
         self.driver.get(self.URL)
 
     def remove_default_filter(self) -> None:
@@ -19,8 +25,47 @@ class YahooCollector:
             "//div[@id='screener-criteria']/div[2]/div[1]/div[1]/div[1]/div[1]/div[2]/ul/li[1]")
         eua_filter.click()
 
+    def find_add_country_filter(self) -> FirefoxWebElement:
+        country_filter_button = self.driver.find_element_by_xpath(
+            "//div[@id='screener-criteria']/div[2]/div[1]/div[1]/div[1]/div[1]/div[2]/ul/li[last()]")
+        return country_filter_button
+
+    @staticmethod
+    def find_input_country_filter(add_filter_country) -> FirefoxWebElement:
+        path_input_country_filter = '//div[1]/div[1]/div[1]/div[1]/div[1]/div[1]/input'
+        input_filter_country = add_filter_country.find_element_by_xpath(path_input_country_filter)
+        return input_filter_country
+
+    @staticmethod
+    def find_check_filter_country(add_filter_country) -> FirefoxWebElement:
+        path_checkbox_country_filter = 'div[1]/div[1]/div[1]/div[1]/div[2]/ul/li/label'
+        checkbox_filter_country = add_filter_country.find_element_by_xpath(path_checkbox_country_filter)
+        return checkbox_filter_country
+
+    def add_country_filter(self):
+        add_filter_country = self.find_add_country_filter()
+        add_filter_country.click()
+        input_filter_country = self.find_input_country_filter(add_filter_country)
+        input_filter_country.send_keys('Argentina')
+        checkbox_filter_country = self.find_check_filter_country(add_filter_country)
+        checkbox_filter_country.click()
+        self.check_filter_country()
+
+    def find_button_find_stocks(self):
+        path_button_find_stocks = "//div[@id='screener-criteria']/div[2]/div[1]/div[3]/button[1]"
+        button_find_stocks = self.driver.find_element_by_xpath(path_button_find_stocks)
+        return button_find_stocks
+
+    def search_stocks(self):
+        button_find_stocks = self.find_button_find_stocks()
+        button_find_stocks.click()
+
     def execute(self) -> None:
+        self.open_site()
         self.remove_default_filter()
+        self.add_country_filter()
+        self.search_stocks()
+        #self.collect_stocks()
 
 
 STRATEGY_COLLECTOR = {
--- stocks/tests/test_collector_yahoo.py
+++ stocks/tests/test_collector_yahoo.py
@@ -5,13 +5,75 @@ from stocks.collector import YahooCollector
 
 
 class YahooCollectorTestCase(unittest.TestCase):
+
     def setUp(self) -> None:
-        pass
+        self.collector = YahooCollector()
 
     def test_remove_default_filter(self) -> None:
-        collector = YahooCollector()
-        collector.remove_default_filter()
-        eua_filter = collector.driver.find_element_by_xpath(
+        self.collector.open_site()
+        self.collector.remove_default_filter()
+        eua_filter = self.collector.driver.find_element_by_xpath(
             "//div[@id='screener-criteria']/div[2]/div[1]/div[1]/div[1]/div[1]/div[2]/ul/li[1]")
         class_filter_eua = 'Bgc($hoverBgColor) Mend(5px) D(ib) Bdrs(3px) filterItem Mb(3px)'
         self.assertIsNot(eua_filter.get_attribute('class'), class_filter_eua)
+
+    def test_open_country_filter_tab(self) -> None:
+        self.collector.open_site()
+        self.collector.remove_default_filter()
+        add_filter_country = self.collector.find_add_country_filter()
+        class_filter_country = 'D(ib) Mb(3px) filterAdd'
+        self.assertEqual(add_filter_country.get_attribute('class'), class_filter_country)
+        add_filter_country.click()
+
+    def test_find_add_country_filter(self) -> None:
+        self.collector.open_site()
+        self.collector.remove_default_filter()
+        add_filter_country = self.collector.find_add_country_filter()
+        class_add_filter_country = 'D(ib) Mb(3px) filterAdd'
+        self.assertEqual(add_filter_country.get_attribute('class'), class_add_filter_country)
+
+    def test_find_input_country_filter(self) -> None:
+        self.collector.open_site()
+        self.collector.remove_default_filter()
+        add_filter_country = self.collector.find_add_country_filter()
+        add_filter_country.click()
+        input_filter_country = self.collector.find_input_country_filter(add_filter_country)
+        class_input_filter_country = 'Bd(0) H(28px) Bgc($lv3BgColor) C($primaryColor) W(100%) Fz(s) Pstart(28px)'
+        self.assertEqual(input_filter_country.get_attribute('class'), class_input_filter_country)
+
+    def test_find_check_country_filter(self) -> None:
+        self.collector.open_site()
+        self.collector.remove_default_filter()
+        add_filter_country = self.collector.find_add_country_filter()
+        add_filter_country.click()
+        input_filter_country = self.collector.find_input_country_filter(add_filter_country)
+        input_filter_country.send_keys('argentina')
+        checkbox_filter_country = self.collector.find_check_filter_country(add_filter_country)
+        class_check_filter_country = 'Fl(start) D(b) Mb(10px)'
+        self.assertEqual(checkbox_filter_country.get_attribute('class'), class_check_filter_country)
+
+    def test_filter_added(self) -> None:
+        self.collector.open_site()
+        self.collector.remove_default_filter()
+        add_filter_country = self.collector.find_add_country_filter()
+        add_filter_country.click()
+        input_filter_country = self.collector.find_input_country_filter(add_filter_country)
+        input_filter_country.send_keys('argentina')
+        checkbox_filter_country = self.collector.find_check_filter_country(add_filter_country)
+        checkbox_filter_country.click()
+        added_filter = self.collector.driver.find_element_by_xpath(
+            "//div[@id='screener-criteria']/div[2]/div[1]/div[1]/div[1]/div[1]/div[2]/ul/li/button/span")
+        self.assertEqual(added_filter.text, 'Argentina')
+
+    def test_find_button_stocks(self) -> None:
+        self.collector.open_site()
+        button_find_stocks = self.collector.find_button_find_stocks()
+        class_button_find_stocks = 'Bgc($linkColor) C(white) Fw(500) Px(20px) Py(9px) Bdrs(3px) Bd(0) Fz(s) D(ib) ' \
+                                   'Whs(nw) Miw(110px) Op(0.3)'
+        self.assertEqual(button_find_stocks.get_attribute('class'), class_button_find_stocks)
+
+    def tearDown(self) -> None:
+        self.collector.driver.close()
+        self.collector.driver.quit()
+
+
